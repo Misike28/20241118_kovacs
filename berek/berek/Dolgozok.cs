f6d0cb339b3f126220ff8d47f6103086f2574f9a
Kovács Mihály <117653038+Misike28@users.noreply.github.com>
2024-11-18 09:03:23 +0100

    Add files via upload

---
+++ berek/berek/Dolgozok.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace berek
+{
+    internal class Dolgozok
+    {
+        public string nev { get; set; }
+        public string neme { get; set; }
+        public string reszleg { get; set; }
+        public int belepes { get; set; }
+        public int ber {  get; set; }
+
+        public override string ToString() =>
+            $"Név: {nev}\n" +
+            $"Nem: {neme}\n" +
+            $"Belépés: {belepes}\n" +
+            $"Bér: {ber} Forint";
+        public Dolgozok(string sor)
+        {
+            string[] v = sor.Split(';');
+            nev = v[0];
+            neme = v[1];
+            reszleg = v[2];
+            belepes = int.Parse(v[3]);
+            ber = int.Parse(v[4]);
+        }
+        }
+}
---
+++ berek/berek/Program.cs
@@ -0,0 +1,33 @@
+﻿using berek;
+using System.Text;
+
+List<Dolgozok> dolgozo = [];
+
+using StreamReader sr = new("..\\..\\..\\src\\berek2020.txt", Encoding.UTF8);
+sr.ReadLine();
+while (!sr.EndOfStream) dolgozo.Add(new(sr.ReadLine()));
+Console.WriteLine($"3. Feladat: dolgozók száma: {dolgozo.Count}");
+
+
+var f4 = dolgozo.Average(d => d.ber) / 1000;
+Console.WriteLine($"4. Feladat: Bérek átlaga: {f4:0.0}eFt");
+
+string input = "";
+Console.Write($"5. Feladat: Kérem a részleg nevét: ");
+input=Console.ReadLine();
+
+var f5= dolgozo.Where(d=>d.reszleg==input)
+    .MaxBy(d=>d.ber);
+if (f5 == null)
+{
+    Console.WriteLine("A megadott részleg nem létezik a cégnél!");
+}
+else
+{
+    Console.WriteLine($"A legtöbbet kereső dolgozó az adott részlegen:\n{f5}");
+}
+
+var f6 = dolgozo.GroupBy(d => d.reszleg);
+Console.WriteLine("7. Feladat: Statisztika");
+foreach (var grp in f6) Console.WriteLine(
+    $"\t{grp.Key} - {grp.Count()} Fő");
